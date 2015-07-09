244af1f95c01a09a1889ca96cb1adaed07b698e3
jzw0020 <jzw0020@users.noreply.github.com>
2015-07-09 02:40:22 -0500

    Delete words_tweeted.py

--- src/words_tweeted.py
+++
@@ -1,8 +0,0 @@
-tweettxt=open('tweets.txt','r')
-tweet=tweettxt.read()  
-ft1=open('ft1.txt','w')
-from collections import Counter
-wordcount = Counter(tweet.split())
-
-for key,value in sorted(wordcount.items()):
-    ft1.write(u'{} {}\n'.format(key, value))
