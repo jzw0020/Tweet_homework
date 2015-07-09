264e2bf3f045f3490a6e7f2732ef85015c9c28eb
jzw0020 <jzw0020@users.noreply.github.com>
2015-07-08 19:59:28 -0500

    Update median_unique.py

--- src/median_unique.py
+++ src/median_unique.py
@@ -4,13 +4,11 @@ def median(lst):
 num_lines=0
 num_words=0
 num_chars=0
-i = 0
 array = []
 ft2=open('ft2.txt','w')
 with open(r'c:\users\jing\onedrive\cpython-ms\tweet\tweets.txt', 'r') as f:
     for line in f:
         words = set(line.split())
-        i +=1
         num_lines += 1
         num_words = len(words)
         num_chars += len(line)
